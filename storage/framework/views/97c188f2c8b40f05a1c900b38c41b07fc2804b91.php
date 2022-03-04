4c2f8fd29c82cc0c6b10cf89d3b87a9c712e14fb
Danna-Aguilar <69810489+Danna-Aguilar@users.noreply.github.com>
2022-03-03 21:16:00 -0600

    Add files via upload

---
+++ storage/framework/views/97c188f2c8b40f05a1c900b38c41b07fc2804b91.php
@@ -0,0 +1,23 @@
+            
+            <?php $__env->startSection('content'); ?>
+            <main class="content">
+            <div class="cards">
+                <div class="card card-center">
+                    <div class="card-body">
+                        <h1>Nueva nota</h1>
+
+                        <form action="">
+                            <label for="title" class="field-label">Título: </label>
+                            <input type="text" name="title" id="title" class="field-input">
+
+                            <label for="content" class="field-label">Contenido:</label>
+                            <textarea name="content" id="content" rows="10" class="field-textarea"></textarea>
+
+                            <button type="submit" class="btn btn-primary">Crear nota</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Marisela\Downloads\NotasLaravel_env\NotasLaravel\resources\views/agregar.blade.php ENDPATH**/ ?>
